03295754d1a9170e62bb5fbb9b4556d5ab2e28e9
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-06-19 12:46:07 +0200

    Init project

---
+++ src/test/java/pl/patrykjava/forecast/ForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykjava.forecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
